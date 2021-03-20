381112ac672637780287e0ebc1711c422c0c26a7
thomas messett <thomas.messett@stampapp.me>
2021-03-20 17:11:12 +0000

    finished api calls added calculator for scoring

--- public/js/script.js
+++ public/js/script.js
@@ -4,31 +4,35 @@ const form = document.getElementById('form')
 // const stations = []
 // stores descriptions of delays taken from TFL API in Delays function
 const serviceDelays = []
+const timeResults = []
+const travelScore = []
 
-// Looks up directions using Google JS API
+
+// Looks up cycle directions using Google JS API
 const cycleDirections = () => {
   const from = document.getElementById('from');
   const to = document.getElementById('to');
-  // For cycling
   fetch(`/cycling-directions/${from.value}/${to.value}`)
     .then(response => response.json())
     .then((data) => {
       const bike = data
       const bikeTime = bike.routes[0].legs[0].duration
       const bikeDistance = bike.routes[0].legs[0].distance
-      console.log(bikeDistance)
+      timeResults.push(bikeTime.value)
     });
 };
 
+
+// Looks up transit directions using Google JS API
 const transitDirections = () => {
   const from = document.getElementById('from');
   const to = document.getElementById('to');
   fetch(`/transit-directions/${from.value}/${to.value}`)
     .then(response => response.json())
     .then((data) => {
-      console.log(data)
       const transit = data
       const transitTime = transit.routes[0].legs[0].duration
+      timeResults.push(transitTime.value);
       const route = transit.routes[0].legs[0].steps
       const lines = []
       for (element of route) {
@@ -46,14 +50,12 @@ const delays = (lines) => {
   fetch('https://api.tfl.gov.uk/line/mode/tube/disruption')
     .then(response => response.json())
     .then((data) => {
-      console.log(data)
       for (element of lines) {
         let re = new RegExp(`${element}`);
-        console.log(re)
         for (element of data) {
           if (re.test(element.description)) {
             serviceDelays.push(element.description)
-            console.log(serviceDelays)
+            travelScore.push(1)
           }
         }
       }
@@ -65,7 +67,14 @@ const airQuality = async () => {
   const api_url = '/airquality';
   const response = await fetch(api_url);
   const json = await response.json();
-  console.log(json);
+  const current = json.currentForecast[0].forecastBand
+  if (current == "Low") {
+    travelScore.push(1)
+  } else if (current == "Moderate") {
+    travelScore.push(0)
+  } else {
+    travelScore.push(-2)
+  }
 };
 
 // Gets weather data from open weather api
@@ -74,31 +83,77 @@ const weatherData = async () => {
   const response = await fetch(api_url);
   const json = await response.json()
     .then((data) => {
+      console.log(data)
       // temperature
       if (data.main.feels_like < 2) {
-        console.log("too cold")
+        travelScore.push(-1)
       } else if (data.main.feels_like > 30) {
-        console.log("too hot")
+        travelScore.push(-1)
       } else {
-        console.log("temp good")
+        travelScore.push(1)
       }
       // wind
       wind = data.wind.speed
       if (wind > 10.8 && wind < 13.9) {
-        console.log("strong winds")
+        travelScore.push(-1)
       } else if (wind > 13.8 && wind < 17.2) {
-      console.log("Very Strong Winds")
-    } else if (wind > 17.1 && wind < 24.5) {
-      console.log("Gale Force Winds")
-    } else if (wind > 24.4) {
-      console.log("Full on Gale")
-    } else
-      console.log("Not too Windy")
+        travelScore.push(-2)
+      } else if (wind > 17.1 && wind < 24.5) {
+        travelScore.push(-3)
+      } else if (wind > 24.4) {
+        travelScore.push(-10)
+      } else {
+        travelScore.push(1)
+      }
       // rain
-
+      rainChance = data.weather[0].main
+      if (rainChance == "Drizzle") {
+        travelScore.push(-1)
+      } else if (rainChance == "Rain") {
+        travelScore.push(-2)
+      } else if (rainChance == "Snow") {
+        travelScore.push(-10)
+      } else if (rainChance == "Thunderstorm"){
+        travelScore.push(-5)
+      } else {
+        travelScore.push(1)
+      }
       // visibility
-      // sunset
+      visibility = data.visibility
+      if (visibility < 101) {
+        travelScore.push(-10)
+      } else if (visibility > 100 && visibility < 1000) {
+        travelScore.push(-5)
+      } else if (visibility > 999 && visibility < 3704) {
+        travelScore.push(-1)
+      } else {
+        travelScore.push(1)
+      }
     });
+}
+
+// work out if cycling or transit is fater
+const fastest = () => {
+  const speed = timeResults[0] - timeResults[1]
+  if (speed >= 0) {
+    travelScore.push(1)
+  } else {
+    travelScore.push(-1)
+  }
+}
+
+// sum results from travelscore array
+const travelSum = () => {
+  let count = 0;
+  console.log(travelScore.length)
+  console.log(travelScore)
+  for(let i = 0; i < travelScore.length; i++)
+  {
+    count = count + travelScore[i];
+    console.log(travelScore[i])
+    console.log(i)
+  }
+  console.log(count)
 }
 
 
@@ -106,6 +161,8 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   cycleDirections()
   transitDirections()
-  airQuality();
-  weatherData();
+  airQuality()
+  weatherData()
+  fastest()
+  travelSum()
 });
